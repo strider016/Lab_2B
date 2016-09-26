7fae1607190ce458c4f50147fc5040c3cc2b75fb
Rasmus <razmus.jansson@gmail.com>
2016-09-26 11:22:48 +0200

    Start shell stuff

---
+++ src/lab2b/Client/Client.java
@@ -0,0 +1,19 @@
+package lab2b.Client;
+
+import java.net.*;
+
+/**
+ * Created by rasmusjansson on 2016-09-26.
+ */
+public class Client {
+    private Socket socket;
+
+    public static void main(String[] args){
+        Client client = new Client();
+        client.Start(args);
+    }
+
+    public void Start(String[] args){
+        //Do very advanced stuff here.
+    }
+}
---
+++ src/lab2b/Server/Server.java
@@ -0,0 +1,15 @@
+package lab2b.Server;
+
+/**
+ * Created by rasmusjansson on 2016-09-26.
+ */
+public class Server {
+    public static void main(String[] args){
+        Server server = new Server();
+        server.Start(args);
+    }
+
+    public void Start(String[] args){
+        //Do very advanced stuff here
+    }
+}
